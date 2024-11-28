f287beb0812276f48e1d4a71cdfc26f765659a97
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2024-11-28 22:54:51 +0800

    if package is pick up then it will disappear

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     bool hasPackage;
+
+    [SerializeField] float floatDestroy = 0.3f;
     void OnCollisionEnter2D(Collision2D other)
     {
         Debug.Log("Ouch!");
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.tag == "Package")
+        if (other.tag == "Package" && !hasPackage)
         {
             hasPackage = true;
             Debug.Log("Package pick up");
+            Destroy(other.gameObject, floatDestroy);
         }
         if (other.tag == "Customer" && hasPackage == true)
         {
